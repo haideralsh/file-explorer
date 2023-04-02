201859f3f31e1bb6b84c0e634a6142a219c26156
Haider Alshamma <haideralsh@gmail.com>
2023-04-02 15:31:55 -0400

    Add Jetbrains Mono

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,7 +13,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500&JetBrains+Mono&display=swap"
             rel="stylesheet"
           />
         </Head>
